e6ff240234c5d0e2cd236de493824b0abbed661b
Sateesh <sateeshkmr.kota@gmail.com>
2019-02-17 19:28:54 +0530

    Command Line Soccer Game.

---
+++ CommandLineSoccer/src/main/java/com/sateekot/soccer/player/functionality/UserForwardFunctionality.java
@@ -0,0 +1,10 @@
+package com.sateekot.soccer.player.functionality;
+
+/**
+ * 
+ * @author skota
+ * Component provides the functionality for User Forward.
+ */
+public class UserForwardFunctionality implements PlayerFunctionality {
+
+}
